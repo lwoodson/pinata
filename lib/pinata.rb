0b96591ba4f119a90ed839c4d81470ec0798b099
lwoodson <lance@webmaneuvers.com>
2013-06-22 13:11:03 -0500

    Documentation and Pinata::Regressions#each method.

--- lib/pinata.rb
+++ lib/pinata.rb
@@ -6,8 +6,11 @@ require 'pinata/ruby'
 module Pinata
   class WhackerFailed < StandardError; end
 
+  # Represents a change in code
   CodeChange = Struct.new(:previous_filepath, :current_filepath)
 
+  # The result of whacking the pinata that is your code by a specific
+  # whacker for some sort of code inspection tool.
   class ResultOfWhacking
     attr_reader :whacker
     attr_accessor :outcome
@@ -28,17 +31,30 @@ module Pinata
     end
   end
 
+  # Represents the regressions introduced with a code change.
   class Regressions
     def initialize
       @data = {}
     end
 
+    # Offers the result to the regression, which will accept it if it
+    # has regressed.
     def offer(result)
       @data[result.whacker] = result if result.regressed?
     end
 
+    # Retrieve the ResultOfWhacking that encapsulates the details of the
+    # regression by its key.
     def [](key)
       @data[key]
+    end
+
+    # Iterates over all accepted regressions, passing each to a supplied
+    # block.
+    def each(&block)
+      @data.keys.each do |key|
+        block.call(@data[key])
+      end
     end
   end
 end
