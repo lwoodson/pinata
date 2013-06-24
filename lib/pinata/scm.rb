b5c17df833414631ff48aba429aaaf6b0ed1d92b
lwoodson <lance@webmaneuvers.com>
2013-06-23 20:16:12 -0500

    Leveraging ruby git gem to do git operations in core code.

--- lib/pinata/scm.rb
+++ lib/pinata/scm.rb
@@ -1,9 +1,14 @@
+require 'git'
+
 module Pinata
   module SCM
     module Git
       def self.current_branch
-        # TODO replace this with ruby git lib
-        `git branch | awk '{print $2}'`.strip
+        git.branch.name 
+        end
+
+      def self.git(dir='.')
+        @git ||= ::Git.open(dir)
       end
     end
   end
