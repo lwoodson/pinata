82176bea45987c707a65bd925cd16ce88cd34eeb
Lance Woodson <lance@webmaneuvers.com>
2013-07-01 04:36:13 -0500

    Removed now spurious Pinata::Regressions class.
    Pinata::Project deals with ResultsOfWhacking directly so it can
    report on improvements as well as regressions.

--- lib/pinata/models.rb
+++ lib/pinata/models.rb
@@ -35,32 +35,4 @@ module Pinata
       outcome == 0
     end
   end
-
-
-  # Represents the regressions introduced with a code change.
-  class Regressions
-    def initialize
-      @data = {}
-    end
-
-    # Offers the result to the regression, which will accept it if it
-    # has regressed.
-    def offer(result)
-      @data[result.whacker] = result if result.regressed?
-    end
-
-    # Retrieve the ResultOfWhacking that encapsulates the details of the
-    # regression by its key.
-    def [](key)
-      @data[key]
-    end
-
-    # Iterates over all accepted regressions, passing each to a supplied
-    # block.
-    def each(&block)
-      @data.keys.each do |key|
-        block.call(@data[key])
-      end
-    end
-  end
 end
--- spec/pinata/ruby_spec.rb
+++ spec/pinata/ruby_spec.rb
@@ -10,22 +10,22 @@ describe Pinata::Ruby do
   describe "#start_whacking" do
     context "when code has gotten better" do
       it "should not contain results from Cane" do
-        regressions = Pinata::Ruby.start_whacking(improving_ruby_code_change)
-        regressions[Pinata::Ruby::Cane].should be_nil
+        results = Pinata::Ruby.start_whacking(improving_ruby_code_change)
+        results.should_not be_empty
       end
     end
 
     context "when code has gotten worse" do
       it "should contain results from Cane" do
-        regressions = Pinata::Ruby.start_whacking(regressing_ruby_code_change)
-        regressions[Pinata::Ruby::Cane].should_not be_nil
+        results = Pinata::Ruby.start_whacking(regressing_ruby_code_change)
+        results.should_not be_empty
       end
     end
 
     context "when code has stayed the same" do
       it "should not contain results from Cane" do
-        regressions = Pinata::Ruby.start_whacking(static_ruby_code_change)
-        regressions[Pinata::Ruby::Cane].should be_nil
+        results = Pinata::Ruby.start_whacking(static_ruby_code_change)
+        results.should_not be_empty
       end
     end
   end
