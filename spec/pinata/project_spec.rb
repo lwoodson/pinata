cbf75b61229c6ea632dea324086c883dc8248d9b
Lance Woodson <lance@webmaneuvers.com>
2013-07-02 07:28:40 -0500

    Ensure Pinata::Project works properly with new files
    * If style violations, note as a project code regression.
    * If no style violations, treat as no change.

--- spec/pinata/project_spec.rb
+++ spec/pinata/project_spec.rb
@@ -19,6 +19,38 @@ describe Pinata::Project do
       end
     end
 
+    context "when encountering a file new to the project" do
+      context "with no detected style violations" do
+        it "should indicate there is a change and that there are no regressions" do
+          in_sandbox do |git|
+            git.create_new_file('test.rb', 'puts "this is a test"')
+            project = Pinata::Project.new
+            project.whack_it!
+            project.should have_changes
+            project.should_not have_regressed
+            project.shift.should == 0
+            project.should_not have_skipped
+            project.should_not have_improved
+          end
+        end
+      end
+
+      context "with detected style violations" do
+        it "should indicate there is a change and regressions" do
+          in_sandbox do |git|
+            git.create_new_file('test.rb', GitRepoHelper::BAD_PLAYER_CONTENTS)
+            project = Pinata::Project.new
+            project.whack_it!
+            project.should have_changes
+            project.should have_regressed
+            (project.shift < 0).should == true
+            project.should_not have_skipped
+            project.should_not have_improved
+          end
+        end
+      end
+    end
+
     context "when encountering a code change to a file for which it has no whacker" do
       it "should indicate that it skipped the file" do
         in_sandbox do |git|
